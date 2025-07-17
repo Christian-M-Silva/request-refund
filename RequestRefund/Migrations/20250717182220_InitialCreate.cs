df578331b7d329add43684fccb0d69f6c014135c
Christian Silva <tininjacms@gmail.com>
2025-07-17 15:26:24 -0300

    feat(migration): Criará e add a migration

---
+++ RequestRefund/Migrations/20250717182220_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RequestRefund.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    EmployeeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    RequestDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
---
+++ RequestRefund/MyDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestRefund.Models.Entities;
+
+namespace RequestRefund
+{
+
+
+    public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
+    {
+        public DbSet<RequestRefundEntity> Requests { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public override int SaveChanges()
+        {
+            UpdateDate();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            UpdateDate();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void UpdateDate()
+        {
+            var now = DateTime.UtcNow;
+
+            foreach (var entry in ChangeTracker.Entries<RequestRefundEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreateAt = now;
+                    entry.Entity.UpdateAt = now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdateAt = now;
+                }
+            }
+        }
+    }
+
+
+
+}
